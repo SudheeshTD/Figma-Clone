9aa8982821859ead2a3f48ce8aa353c6ba4f2504
Sudheesh T D <sudheeshtd01@gmail.com>
2025-04-24 11:20:56 -0500

    cursor tracking

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
+"use client";
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
-    <div
-      className="h-[100vh]
-      w-full
-      flex
-      justify-center
-      items-center
-      text-center"
-    >
-      <h1 className="text-2xl text-white">LiveBlocks</h1>
+    <div>
+      <Live />
     </div>
   );
 }
--- components/Live.tsx
+++ components/Live.tsx
@@ -1,12 +1,66 @@
-import React from "react";
+"use client";
+import { useCallback } from "react";
 import LiveCursors from "./cursor/LiveCursors";
-import { useOthers } from "@liveblocks/react";
+import { useMyPresence, useOthers } from "@liveblocks/react";
 
 const Live = () => {
   const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    // if cursor is not in reaction selector mode, update the cursor position
+    // if (cursor == null || cursorState.mode !== CursorMode.ReactionSelector) {
+    // get the cursor position in the canvas
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    // broadcast the cursor position to other users
+    updateMyPresence({
+      cursor: {
+        x,
+        y,
+      },
+    });
+    // }
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    updateMyPresence({
+      cursor: null,
+      message: null,
+    });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    // get the cursor position in the canvas
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({
+      cursor: {
+        x,
+        y,
+      },
+    });
+  }, []);
 
   return (
-    <div>
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh]
+      w-full
+      flex
+      justify-center
+      items-center
+      text-center"
+    >
+      <h1 className="text-2xl text-white">LiveBlocks</h1>
       <LiveCursors others={others} />
     </div>
   );
