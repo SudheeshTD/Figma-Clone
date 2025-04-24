15ccf69fd15003c0ac7e870c2ae80ab286f72ff8
Sudheesh T D <sudheeshtd01@gmail.com>
2025-04-24 01:58:36 -0500

    adding center of app

---
+++ components/cursor/Cursor.tsx
@@ -0,0 +1,14 @@
+
+type Props = {
+    color: string;
+    x: number;
+    y: number;
+    message?: string;
+  };
+  
+
+const Cursor = ({ color, x, y, message }: Props) => (
+  return <div>Cursor</div>;
+};
+
+export default Cursor;
