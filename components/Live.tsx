bd0c5a537dabcd1ef06e768fe25b4c52f7c9f5ef
Sudheesh T D <sudheeshtd01@gmail.com>
2025-04-25 13:57:46 -0500

    Text on cursor available only when pressed /

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import LiveCursors from "./cursor/LiveCursors";
 import { useMyPresence, useOthers } from "@liveblocks/react";
 import { CursorMode } from "@/types/type";
@@ -51,6 +51,34 @@ const Live = () => {
       },
     });
   }, []);
+
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: "",
+        });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ message: "" });
+        setCursorState({ mode: CursorMode.Hidden });
+      }
+    };
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
 
   return (
     <div
--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,5 +1,5 @@
 import CursorSVG from "@/public/assets/CursorSVG";
-import { CursorChatProps } from "@/types/type";
+import { CursorChatProps, CursorMode } from "@/types/type";
 import React from "react";
 
 const CursorChat = ({
@@ -17,7 +17,20 @@ const CursorChat = ({
     });
   };
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {};
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setCursorState({
+        mode: CursorMode.Chat,
+        // @ts-ignore
+        previousMessage: cursorState.message,
+        message: "",
+      });
+    } else if (e.key === "Escape") {
+      setCursorState({
+        mode: CursorMode.Hidden,
+      });
+    }
+  };
 
   return (
     <div
@@ -26,31 +39,33 @@ const CursorChat = ({
         transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
       }}
     >
-      <>
-        {/* Custom Cursor shape */}
-        <CursorSVG color="#000" />
+      {cursorState.mode === CursorMode.Chat && (
+        <>
+          {/* Custom Cursor shape */}
+          <CursorSVG color="#000" />
 
-        <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
-          {/**
-           * if there is a previous message, show it above the input
-           *
-           * We're doing this because when user press enter, we want to
-           * show the previous message at top and the input at bottom
-           */}
-          {cursorState.previousMessage && (
-            <div>{cursorState.previousMessage}</div>
-          )}
-          <input
-            className="z-10 w-60 border-none	bg-transparent text-white placeholder-blue-300 outline-none"
-            autoFocus={true}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            placeholder={cursorState.previousMessage ? "" : "Say something…"}
-            value={cursorState.message}
-            maxLength={50}
-          />
-        </div>
-      </>
+          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+            {/**
+             * if there is a previous message, show it above the input
+             *
+             * We're doing this because when user press enter, we want to
+             * show the previous message at top and the input at bottom
+             */}
+            {cursorState.previousMessage && (
+              <div>{cursorState.previousMessage}</div>
+            )}
+            <input
+              className="z-10 w-60 border-none	bg-transparent text-white placeholder-blue-300 outline-none"
+              autoFocus={true}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+              placeholder={cursorState.previousMessage ? "" : "Say something…"}
+              value={cursorState.message}
+              maxLength={50}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
